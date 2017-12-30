88b060f8b64266ebf79adb8ddda17660b5b25378
Johnson <240394381@qq.com>
2017-12-30 11:47:12 +0800

    Initial Commit

---
+++ AIDesign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AIDesign
+//
+//  Created by xinying on 2017/12/30.
+//  Copyright © 2017年 habav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
